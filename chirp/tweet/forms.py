3c503ba9148443eb386825ce80cd713313e87eb3
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-11-15 12:12:54 +0530

    add tweet_list, create,edit tweet form

--- chirp/tweet/forms.py
+++ chirp/tweet/forms.py
@@ -5,4 +5,12 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'class': 'form-control rounded border border-secondary shadow-sm p-3',
+                'rows': 5,
+                'placeholder': "What's on your mind?",
+                'style': 'resize: none;',
+            }),
+        }
         
