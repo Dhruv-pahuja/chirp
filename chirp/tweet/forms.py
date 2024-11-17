a8c070aeed67141f07d60f7af2e01faefea8d776
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-11-17 12:52:58 +0530

    added user registration and login

--- chirp/tweet/forms.py
+++ chirp/tweet/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Tweet
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
@@ -14,3 +15,25 @@ class TweetForm(forms.ModelForm):
             }),
         }
         
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control rounded border border-secondary shadow-sm p-3',
+                'placeholder': 'Username',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control rounded border border-secondary shadow-sm p-3',
+                'placeholder': 'Email',
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control rounded border border-secondary shadow-sm p-3',
+                'placeholder': 'Password',
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control rounded border border-secondary shadow-sm p-3',
+                'placeholder': 'Confirm Password',
+            }),
+        }
